33345b776e14cf3ba8f0808e8f17fc9a3d23a54c
osannikova <h.sannikova@gmail.com>
2019-12-04 17:15:38 +0000

    sample code

---
+++ js/index.js
@@ -0,0 +1,39 @@
+//декларуємо змінні
+//var-старе, не використовуємо
+//let-звичайна змінна
+//const-незмінна (постійна, константа, стала) змінна
+
+//Primitive Data Types
+//Number
+//String
+//Boolean - true, false
+//null - пусто
+//undefined - ек визначено
+//Object - об'єкт
+//Symbol - унікальний незмінний ідентифікатор
+
+let bestGreetings = 'Hello';
+alert(bestGreetings);
+bestGreetings = 'JavaScript';
+alert(bestGreetings);
+
+const age = Number (prompt ('Enter your age'));
+alert('Your age is:' + age);
+
+const nextAge = addOne(age);
+alert('Next year you will be:' + nextAge);
+
+//Logical operators
+//< > <= >= != !== === || &&
+
+if (age >= 18) {
+    alert('You are adult');
+} else {
+    alert('You are underaged');
+}
+function addOne(num) {
+    const incremented = num + 1;
+    return incremented;
+
+}
+
